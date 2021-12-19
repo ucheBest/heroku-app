405d92cca08bcfb45928d87c92758cc80dd8d168
ucheBest <akah.uchechi@gmail.com>
2021-12-19 23:13:15 +0000

    Create functional logic for heroku app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import { fetchData, ProductList } from './api/products';
+import { Filters } from './components/filters/filters';
+import { ProductTable } from './components/product-table/product-table';
+
+export interface IProducts{
+  unfilteredProducts: ProductList[];
+  filterdProducts: ProductList[];
+  setFilteredProducts: (value: React.SetStateAction<ProductList[]>) => void
+}
 
 function App() {
+  
+  const [products, setProducts] = useState<ProductList[]>([]);
+  const [filterdProducts, setFilteredProducts] = useState<ProductList[]>([]);
+
+  useEffect(() => {
+    const initialiseProducts = async () => {
+      const res = await fetchData();
+      setProducts(res);
+      setFilteredProducts(res);
+    }
+    initialiseProducts() 
+   
+  }, []);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Welcome!</h1>
+     <Filters 
+        unfilteredProducts={products} 
+        filterdProducts={filterdProducts}
+        setFilteredProducts={setFilteredProducts}      
+        />
+      <ProductTable products={filterdProducts} />
     </div>
   );
 }
